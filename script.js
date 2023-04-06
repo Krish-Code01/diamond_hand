3fe969296433a16da359b4e3d2328218c7cb06b3
Krishna Rajput <krishnarajput1027@gmail.com>
2023-04-06 12:28:44 +0530

    First Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("mousemove", function () {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(function () {
     front.classList.add("animate");
-  }, 100); // Add class after 1 second of no mouse movement
+  }, 1000); // Add class after 1 second of no mouse movement
 });
 
 function parallax(e) {
@@ -12,8 +12,8 @@ function parallax(e) {
     var front = document.getElementById("front");
     front.classList.remove("animate");
     var moving_value = move.getAttribute("data-value");
-    var x = (e.clientX * moving_value) / 200; // First variable x
-    var y = (e.clientY * moving_value) / 200; // Second variable should be y, not x
+    var x = (e.clientX * moving_value) / 150; // First variable x
+    var y = (e.clientY * moving_value) / 150; // Second variable y
     move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
   });
 }
